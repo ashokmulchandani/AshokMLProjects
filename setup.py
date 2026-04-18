2e54c5b8d8a6631741edae7dbe8467641b181d6e
Ashok Mulchandani <ashokmulchandani@gmail.com>
2026-04-18 22:29:12 +1000

    serc folder setup  setup

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import find_packages, setup
 
+def get_requirements(file_path):
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirement]
+        
+    return requirements
+
+HYPEN_E_DOT='-e.'
+
+if HYPEN_E_DOT in requirement:
+        requirement.remove(HYPEN_E_DOT)
 setup(
     name='mlproject',
     version='0.0.1',
     packages=find_packages(),
+    auther='Ashok Mulchandani',
+    auther_email='ashokmulchandani@gmail.com'
+    install_requires=get_requirements('requirement.txt')       
 )
