ca545c3b0947cee68af95dd0c6c382a81f3b270c
Ashok Mulchandani <ashokmulchandani@gmail.com>
2026-04-20 13:50:28 +1000

    Data Ingestion setup

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ def get_requirements(file_path):
     '''
     This function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirement]
-        
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    HYPEN_E_DOT = '-e .'
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
     return requirements
 
-HYPEN_E_DOT='-e.'
-
-if HYPEN_E_DOT in requirement:
-        requirement.remove(HYPEN_E_DOT)
 setup(
     name='mlproject',
     version='0.0.1',
     packages=find_packages(),
-    auther='Ashok Mulchandani',
-    auther_email='ashokmulchandani@gmail.com'
-    install_requires=get_requirements('requirement.txt')       
+    author='Ashok Mulchandani',
+    author_email='ashokmulchandani@gmail.com',
+    install_requires=get_requirements('requirement.txt')
 )
