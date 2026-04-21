4a61989321ed3e74f740678d282e86380d0a6aa7
Ashok Mulchandani <ashokmulchandani@gmail.com>
2026-04-21 17:06:53 +1000

    Revert "Setup Model Trainer"
    This reverts commit 80faec41a0fb6f16d1971abc1493ebe0319a6302.

--- src/components/data_ingestion_1.py
+++ src/components/data_ingestion_1.py
@@ -11,9 +11,6 @@ from dataclasses import dataclass
 from src.components.data_transformation_1 import DataTransformation
 from src.components.data_transformation_1 import DataTransformationConfig
 
-from src.components.model_trainer_1 import ModelTrainerConfig
-from src.components.model_trainer_1 import ModelTrainer
-
 
 @dataclass
 class DataIngestionConfig:
@@ -52,10 +49,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data, test_data=obj.initiate_data_ingestion()
+    train_data, test_ddata=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
-
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.inititate_model_trainer(train_arr, test_arr, preprocessor_path=None))
+    data_transformation.initiate_data_transformation(train_data, test_ddata)
